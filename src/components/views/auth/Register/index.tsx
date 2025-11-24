7ce62dfd3dda2fd97e288ad558fd849cb625d21b
inovafani <inoveka@gmail.com>
2025-11-24 13:39:21 +0700

    feat: register

--- src/components/views/auth/Register/index.tsx
+++ src/components/views/auth/Register/index.tsx
@@ -1,12 +1,48 @@
+import { FormEvent, use, useState } from 'react';
 import styles from './Register.module.scss';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const RegisterView = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+  const { push } = useRouter();
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
+    setError('');
+    const form = event.target as HTMLFormElement;
+    const data = {
+      email: form.email.value,
+      fullname: form.fullname.value,
+      phone: form.phone.value,
+      password: form.password.value,
+    };
+
+    const result = await fetch('/api/user/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (result.status === 200) {
+      form.reset();
+      setIsLoading(false);
+      push('/auth/login');
+    } else {
+      setIsLoading(false);
+      setError('Email is already registered');
+      console.log('error');
+    }
+  };
   return (
     <div className={styles.register}>
       <h1 className={styles.register__title}>Register</h1>
+      {error && <p className={styles.register__error}>{error}</p>}
       <div className={styles.register__form}>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <div className={styles.register__form__item}>
             <label htmlFor="email">Email</label>
             <input
@@ -43,7 +79,9 @@ const RegisterView = () => {
               className={styles.register__form__item__input}
             />
           </div>
-          <button className={styles.register__form__button}>Register</button>
+          <button type="submit" className={styles.register__form__button}>
+            {isLoading ? 'Loading...' : 'Register'}
+          </button>
         </form>
       </div>
       <p className={styles.register__link}>
